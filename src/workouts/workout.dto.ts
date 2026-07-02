a7b3cb7a18cb9b636da89f549cd3387f0d2efb6f
sejung2 <seingdev@gmail.com>
2026-07-02 18:00:25 +0900

    feat: add workout dto

---
+++ src/workouts/workout.dto.ts
@@ -0,0 +1,26 @@
+export enum WorkoutType {
+  GYM = 'GYM',
+  SPORTS = 'SPORTS',
+}
+
+export enum BodyPart {
+  CHEST = 'CHEST',
+  BACK = 'BACK',
+  LEGS = 'LEGS',
+  SHOULDERS = 'SHOULDERS',
+  BICEPS = 'BICEPS',
+  TRICEPS = 'TRICEPS',
+}
+
+export class CreateWorkoutDto {
+  workoutType!: WorkoutType;
+  bodyParts?: BodyPart[];
+}
+
+export interface WorkoutCheck {
+  id: number;
+  workoutType: WorkoutType;
+  bodyParts?: BodyPart[];
+  date: string;
+  createdAt: Date;
+}
